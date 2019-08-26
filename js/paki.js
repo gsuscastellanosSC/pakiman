ef4cfab6f8890c87d0e6ef69d6e5b547c7a3051a
SC <jesuscastellanospaez@gmail.com>
2019-08-25 19:27:58 -0500

    Hasta el momento aprendi como usar de arreglos, crear clases, instanciar objetos escribir con "appendChild" y un for simplificado con el que si se usa in: mustra el indice del arreglo; of: Muestra el objeto en el arreglo

---
+++ js/paki.js
@@ -0,0 +1,13 @@
+var imagenes = [];
+imagenes["Cauchin"] = "img/vaca.png";
+imagenes["Pokacho"] = "img/pollo.png";
+imagenes["Tocinauro"] = "img/cerdo.png";
+
+var coleccion = [];
+coleccion.push(new Pakiman("Tocinauro", 120, 40));
+coleccion.push(new Pakiman("Pokacho", 80, 50));
+coleccion.push(new Pakiman("Cauchin", 100 ,30));
+
+for (var p of coleccion){   /* in: mustra el indice; of: Muestra el objeto*/
+  p.mostrar();
+}
